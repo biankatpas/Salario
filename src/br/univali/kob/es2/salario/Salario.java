e846a314cdf5e7d23832c4b4c69d932a585bfe28
Bianka Passos <biankatpas@gmail.com>
2017-10-29 00:30:03 -0200

    initial commit

---
+++ src/br/univali/kob/es2/salario/Salario.java
@@ -0,0 +1,18 @@
+
+package br.univali.kob.es2.salario;
+
+/**
+ *
+ * @author biankatpas
+ * @author evelynrissi
+ */
+public class Salario {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
